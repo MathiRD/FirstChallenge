f3bf592699312017b65a48f3a762df115c86da20
Matheus D <matheusdurigon1@gmail.com>
2023-07-17 17:36:35 -0300

    fix: The expand button and some adjusts about the information of the comments

--- post.ts
+++ post.ts
@@ -87,14 +87,14 @@ const comments: comment[] = [
         postId: 1,
         username: "user8",
         email: "user8@example.com",
-        body: "hey man whats up?!"
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         id: 8,
         postId: 6,
         username: "user9",
         email: "user9@example.com",
-        body: "oh this is not nice man..."
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
 ];
 
